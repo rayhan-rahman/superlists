63221999b3af674935f4ddfcd14d8140a9dd76ed
Rayhan Rahman <rayhan_rahman@hotmail.com>
2019-12-17 14:02:51 -0800

    added unit test NewVistorTest and a failing test test_can_start_a_list_and_retrieve_it_latetr

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
-
-browser = webdriver.Chrome()
-
-browser.get('http://localhost:8000')
+import unittest
 
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Theo wants to create a new To-Do lists; so, he visits
+        self.browser.get('http://localhost:8000')
+        # Theo notices page title and header mention "To-Do" lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == '__main__':
+    unittest.main()
