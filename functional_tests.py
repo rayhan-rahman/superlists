3b207563b45b9ad2dd0cf3a04a1bd50ee42a215f
Rayhan Rahman <rayhan_rahman@hotmail.com>
2019-12-19 13:42:17 -0800

    added some more functional tests, an user is invited to enter an item and then upon entering and pressing enter button the page should refresh and the previously entered item should be shown on the page

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
+import time
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -14,6 +16,25 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         # Theo notices page title and header mention "To-Do" lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        # Theo is invited to add a item straight-away
+        input_box = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        # Theo types "Buy peacock feathers" into a text box
+        input_box.send_keys('Buy peacock feathers')
+        # When Theo hits enter the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
         self.fail('Finish the test!')
 
 
