b8bc827b124a1c463a2dac5b601404091e68a0ac
Rayhan Rahman <rayhan_rahman@hotmail.com>
2019-12-20 12:22:39 -0800

    refactored functional test

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Theo wants to create a new To-Do lists; so, he visits
         self.browser.get('http://localhost:8000')
@@ -30,9 +35,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list table
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         # There's still a textbox that invites Theo to enter
         # another item in the to-do list
         # Theo enters "Use peacock feathers to make a fly"
@@ -42,11 +45,9 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         # Theo notices that the page updates and he
         # sees both the items on the list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
 
